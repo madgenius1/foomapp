ad6df4a9621b9495a208f1845392d6ad346e7d28
ME <nickjuma@outlook.com>
2025-09-15 13:21:59 +0300

    android/build.gradle.kts         modified:   android/gradle.properties         modified:   android/gradle/wrapper/gradle-wrapper.properties

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     classpath 'com.google.gms:google-services:4.4.2'
 }
 
+
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
